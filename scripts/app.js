b515c1b94284b63a4ae0c1fb117ca4549aedaeba
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-03-24 07:29:45 -0700

    Still can't get the color changer bonus part

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,79 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 var $quakesList;
 var map;
+var quakeMagnitude;
+var magnitudeNumber;
+
+function timeSince(seconds) {
+  var yearsAgo = Math.floor(((new Date() - seconds)/1000) / 31536000);
+  var daysAgo = Math.floor((((new Date() - seconds)/1000) % 31536000) / 86400);
+  var hoursAgo = Math.floor(((((new Date() - seconds)) % 31536000) % 86400) / 3600);
+  var minutesAgo = Math.floor((((((new Date() - seconds)/1000) % 31536000) % 86400) % 3600) / 60);
+  var secondsAgo = (((((new Date() - seconds)/1000) % 31536000) % 86400) % 3600) % 60;
+  return yearsAgo + " years " +  daysAgo + " days " + hoursAgo + " hours " + minutesAgo + " minutes " + secondsAgo.toFixed(3) + " seconds";
+  }
 
 $(document).on("ready", function() {
 
   var source = $('#earthquakes-template').html();
   var template = Handlebars.compile(source);
+  var geoMarkers = [];
+
+  Handlebars.registerHelper('yearsAgo', function(time) {
+    var yearsAgo = Math.floor(((new Date() - time)/1000) / 31536000);
+    return yearsAgo + ' years';
+  });
+  Handlebars.registerHelper('daysAgo', function(time) {
+    var daysAgo = Math.floor((((new Date() - time)/1000) % 31536000) / 86400);
+    return daysAgo + ' days';
+  });
+  Handlebars.registerHelper('hoursAgo', function(time) {
+    var hoursAgo = Math.floor(((((new Date() - time)) % 31536000) % 86400) / 3600);
+    return hoursAgo + ' hours';
+  });
+  Handlebars.registerHelper('minutesAgo', function(time) {
+    var minutesAgo = Math.floor((((((new Date() - time)/1000) % 31536000) % 86400) % 3600) / 60);
+    return minutesAgo + ' minutes';
+  });
+  Handlebars.registerHelper('secondsAgo', function(time) {
+    var secondsAgo = Math.floor((((((new Date() - time)/1000) % 31536000) % 86400) % 3600) / 60);
+    return secondsAgo + ' seconds';
+  });
+
+  Handlebars.registerHelper('nameSlicer', function(quakeName) {
+    var substringName = quakeName.substring(quakeName.indexOf("of") + 3, quakeName.length);
+    // var locationName = offName.substring(4, quakeName.length);
+    return substringName;
+  });
+
+  function makeRed() {
+    $(".magnitude").css("background-color","red");
+    console.log($(".magnitude").text());
+  }
+  function makeOrange() {
+    $(".magnitude").text('<h3 class="orangeColor">{{properties.mag}}</h3>');
+    console.log($(".magnitude").text());
+  }
+  function makeYellow() {
+    $(".magnitude").text('<h3 class="yellowColor">{{properties.mag}}</h3>');
+    console.log($(".magnitude").text());
+  }
+
+  function colorPicker(magNumber) {
+
+    if(magNumber >= 6) {
+      // console.log("red");
+      makeRed();
+
+    } else if(magNumber >= 5) {
+      // console.log("orange");
+      makeOrange();
+
+    } else if(magNumber <= 5) {
+      // console.log("yellow");
+      makeYellow();
+    }
+  }
 
   function getQuakeData() {
     var response = $.ajax({
@@ -16,7 +84,6 @@ $(document).on("ready", function() {
       error: onError
     });
   }
-  var geoMarkers = [];
 
   function onSuccess(data) {
 
@@ -25,20 +92,15 @@ $(document).on("ready", function() {
 
     geoQuakes.forEach(function(quake){
 
+      magnitudeNumber = parseFloat(quake.properties.mag);
+      colorPicker(magnitudeNumber);
+
       if(quake.geometry.coordinates) {
         var lng = quake.geometry.coordinates[1];
         var lat = quake.geometry.coordinates[0];
         geoMarkers.push([lng,lat]);
       }
     });
-
-    console.log(geoMarkers);
-
-    // var specificGeoQuake = data.features[0].properties.title;
-    // var magnitude = data.features[0].properties.mag;
-    // var latitude = data.features[0].geometry.coordinates[0];
-    // var longitude = data.features[0].geometry.coordinates[1];
-    // var zoom = data.features[0].geometry.coordinates[2];
 
     var earthquakeHtml = template({
       earthquakes: geoQuakes,
@@ -57,11 +119,10 @@ $(document).on("ready", function() {
         var marker = new google.maps.Marker({
           position: new google.maps.LatLng(geoMarkers[i][0],geoMarkers[i][1]),
           map: map,
-          title: "Test Map"
+          title: "Test Map",
         });
       }
     }
-
     initMap();
   }
 
@@ -71,7 +132,5 @@ $(document).on("ready", function() {
     console.log("Status: " + status);
     console.dir(xhr);
   }
-
   getQuakeData();
-
 });
